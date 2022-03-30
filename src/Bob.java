a78d052aaf91275f9b281e5a070d5a34ed6d2695
Samuel Arguello <sam.rene12@gmail.com>
2022-03-30 10:30:48 -0500

    completed bob.java

--- src/Bob.java
+++ src/Bob.java
@@ -4,20 +4,22 @@ public class Bob {
     public static void main (String[] args){
 
         Scanner sc = new Scanner(System.in);
-
         String userInput = sc.nextLine();
-//        System.out.println(userInput);
-        char lastChar = userInput.charAt(userInput.length() - 1);
-//        System.out.println(lastChar);
+        System.out.println("Say hello to bob!");
 
         if(userInput.equals("")){
-            System.out.println("Fine. Be that way!");
-        } else if (Character.toString(lastChar).matches("[?]")){
-            System.out.println("sure...");
-        } else if(Character.toString(lastChar).matches("[!]")){
-            System.out.println("Whoa, chill out man!");
+            System.out.println("Fine. Be that way!!");
         } else {
-            System.out.println("whatever...");
+            char lastChar = userInput.charAt(userInput.length() - 1);
+
+            if(Character.toString(lastChar).matches("[?]")){
+                System.out.println("sure...");
+            } else if (Character.toString(lastChar).matches("[!]")) {
+                System.out.println("Whoa, chill out man!");
+            } else {
+                System.out.println("whatever...");
+            }
+
         }
     }
 }
