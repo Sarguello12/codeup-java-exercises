1e6aefe6e464bf53e7e23b036ddbefacb24500e3
Samuel Arguello <sam.rene12@gmail.com>
2022-04-04 14:01:25 -0500

    began work on movies

---
+++ src/Movies/Movies.java
@@ -0,0 +1,17 @@
+package Movies;
+
+public class Movies {
+    private String name;
+    private String category;
+
+    public Movies(String name, String category){
+        this.name = name;
+        this.category = category;
+    }
+}
+
+class MoviesApplication {
+    public static void main(String[] args) {
+
+    }
+}
