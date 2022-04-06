2f1216dc05f72ef41e1cde417b8148017a01f32c
Samuel Arguello <sam.rene12@gmail.com>
2022-04-06 09:49:25 -0500

    completed functionality of Student.java, addGrade, getAverageGrade, getters and setters

--- src/Grades/Student.java
+++ src/Grades/Student.java
@@ -1,6 +1,7 @@
 package Grades;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Student {
     private String studentName;
@@ -11,17 +12,29 @@ public class Student {
         this.grades = grades;
     }
 
-    // returns the student's name
-    public String getName() {
-        return studentName;
-    }
-
     // adds the given grade to the grades property
     public void addGrade(int grade) {
         grades.add(grade);
     }
 
     // returns the average of the students grades
-    public double getGradeAverage() {
+    private int getGradeAverage(List <Integer> grades) {
+        Integer sum = 0;
 
+        for (Integer grade : grades) {
+            sum += grade;
+        }
+
+        return sum / grades.size();
     }
+
+    // returns the student's name
+    public String getName() {
+        return studentName;
+    }
+
+    // returns the student's grades
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+}
